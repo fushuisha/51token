7f1fbb732876ba6c894daa8d4b82162795c27e1f
沙大为 <38444964@qq.com>
2019-07-02 16:59:46 +0800

    添加PwdController

---
+++ src/main/java/com/token51/PwdController.java
@@ -0,0 +1,111 @@
+package com.token51;
+
+import com.token51.util.CommonUtils;
+import com.token51.util.ConstUtils;
+import com.token51.util.JacksonUtils;
+import com.token51.util.JsonResult;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.GenericValidator;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@RestController
+public class PwdController {
+
+    private final String typeOpenssl = "openssl";
+    private final String typeGpg = "gpg";
+    private final String typeGpg2 = "gpg2";
+    private final String typeEasy = "easy";
+    private final String typeNormal = "normal";
+    private final String typeHard = "hard";
+
+    /**
+     * http://127.0.0.1:8080/pwdgen?type=openssl&length=14
+     *
+     * @return
+     */
+    @RequestMapping(value = "/pwdgen")
+    public String pwdgen(@RequestParam(required = false) String type,
+                               @RequestParam(required = false) String length) {
+        type = StringUtils.trimToEmpty(type);
+        int lengthInt = 10;
+        if (StringUtils.isNotBlank(length) && GenericValidator.isInt(length)) {
+            lengthInt = Integer.parseInt(length);
+        }
+        if (lengthInt > 16) {
+            lengthInt = 10;
+        }
+        String dataResult;
+        switch (type) {
+            case typeNormal: {
+                String cmd =  "pwgen -s " + lengthInt+" 1";
+                dataResult = CommonUtils.executeCmd(cmd);
+                break;
+            }
+            case typeHard: {
+                String cmd =  "pwgen -cnys " + lengthInt+" 1";
+                dataResult = CommonUtils.executeCmd(cmd);
+                break;
+            }
+            default: {
+                // easy
+                String cmd = "pwgen " + lengthInt+" 1";
+                dataResult = CommonUtils.executeCmd(cmd);
+                break;
+            }
+        }
+        JsonResult result = JacksonUtils.genJsonResult(ConstUtils.SUCCESS, dataResult);
+        return JacksonUtils.callback(CommonUtils.getCurrentRequest(), JacksonUtils.toJson(result));
+    }
+
+    /**
+     * http://127.0.0.1:8080/pwdgenBase64?type=openssl&length=14
+     *
+     * @return
+     */
+    @RequestMapping(value = "/pwdgenBase64")
+    public String pwdgenBase64(@RequestParam(required = false) String type,
+                               @RequestParam(required = false) String length) {
+        type = StringUtils.trimToEmpty(type);
+        int lengthInt = 10;
+        if (StringUtils.isNotBlank(length) && GenericValidator.isInt(length)) {
+            lengthInt = Integer.parseInt(length);
+        }
+        if (lengthInt > 16) {
+            lengthInt = 10;
+        }
+        String dataResult;
+        switch (type) {
+            case typeOpenssl: {
+                String cmd = "openssl rand -base64 " + lengthInt;
+                dataResult = CommonUtils.executeCmd(cmd);
+                break;
+            }
+            case typeGpg: {
+                String cmd = "gpg --gen-random --armor 1 " + lengthInt;
+                dataResult = CommonUtils.executeCmd(cmd);
+                break;
+            }
+            case typeGpg2: {
+                String cmd = "gpg2 --gen-random --armor 1 " + lengthInt;
+                dataResult = CommonUtils.executeCmd(cmd);
+                break;
+            }
+            default: {
+                // java
+                dataResult = Base64.encodeBase64String(((System.currentTimeMillis() * (new Random().nextInt(99) + 2)) + "").getBytes());
+                dataResult = StringUtils.substring(dataResult, lengthInt * -1);
+                break;
+            }
+        }
+        JsonResult result = JacksonUtils.genJsonResult(ConstUtils.SUCCESS, dataResult);
+        return JacksonUtils.callback(CommonUtils.getCurrentRequest(), JacksonUtils.toJson(result));
+    }
+
+}
--- src/main/java/com/token51/util/CommonUtils.java
+++ src/main/java/com/token51/util/CommonUtils.java
@@ -5,11 +5,21 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CommonUtils {
+    public static void main(String[] args) throws Exception  {
+        String cmd = "ping www.baidu.com";
+        String result = executeCmd(cmd);
+        System.out.println(result);
+    }
     public static HttpServletRequest getCurrentRequest() {
         try {
             HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
@@ -48,5 +58,32 @@ public class CommonUtils {
         }
         active = System.getenv(ConstUtils.SPRING_PROFILES_ACTIVE);
         return active;
+    }
+
+    public static String executeCmd(String cmd) {
+        Runtime rt = Runtime.getRuntime(); // 运行时系统获取
+//        Map<String, String> lineMap = new HashMap<String, String>();//存放返回值
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            Process proc = rt.exec(cmd);// 执行命令
+            InputStream stderr = proc.getInputStream();//执行结果 得到进程的标准输出信息流
+            Thread.sleep(5000);
+            InputStreamReader isr = new InputStreamReader(stderr);//将字节流转化成字符流
+            BufferedReader br = new BufferedReader(isr);//将字符流以缓存的形式一行一行输出
+            while (br.ready()) {
+                String line = br.readLine();
+                stringBuilder.append(line + "\n");
+            }
+            br.close();
+            isr.close();
+            stderr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String obj2str (Object obj) {
+        return obj == null ? "":obj.toString();
     }
 }
