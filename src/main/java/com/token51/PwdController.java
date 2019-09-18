21eb4b2a3f0e998a1fa29cdae4ef35eea7675e02
david <38444964@qq.com>
2019-09-18 15:48:48 +0800

    生成密码

--- src/main/java/com/token51/PwdController.java
+++ src/main/java/com/token51/PwdController.java
@@ -40,7 +40,7 @@ public class PwdController {
         if (StringUtils.isNotBlank(length) && GenericValidator.isInt(length)) {
             lengthInt = Integer.parseInt(length);
         }
-        if (lengthInt > 16) {
+        if (lengthInt < 6 || lengthInt > 16) {
             lengthInt = 10;
         }
         String dataResult;
@@ -56,36 +56,12 @@ public class PwdController {
                 logger.warn(dataResult);
                 break;
             }
-            default: {
-                // easy
+            case typeEasy: {
                 String cmd = "pwgen " + lengthInt+" 1";
                 dataResult = CommonUtils.executeCmd(cmd);
+                logger.warn(dataResult);
                 break;
             }
-        }
-        dataResult = dataResult.replace("\n","").replace("<","[").replace(">","]");
-        JsonResult result = JacksonUtils.genJsonResult(ConstUtils.SUCCESS, dataResult);
-        return JacksonUtils.callback(CommonUtils.getCurrentRequest(), JacksonUtils.toJson(result));
-    }
-
-    /**
-     * http://127.0.0.1:8080/pwdgenBase64?type=openssl&length=14
-     *
-     * @return
-     */
-    @RequestMapping(value = "/pwdgenBase64")
-    public String pwdgenBase64(@RequestParam(required = false) String type,
-                               @RequestParam(required = false) String length) {
-        type = StringUtils.trimToEmpty(type);
-        int lengthInt = 10;
-        if (StringUtils.isNotBlank(length) && GenericValidator.isInt(length)) {
-            lengthInt = Integer.parseInt(length);
-        }
-        if (lengthInt > 16) {
-            lengthInt = 10;
-        }
-        String dataResult;
-        switch (type) {
             case typeOpenssl: {
                 String cmd = "openssl rand -base64 " + lengthInt;
                 dataResult = CommonUtils.executeCmd(cmd);
@@ -108,7 +84,7 @@ public class PwdController {
                 break;
             }
         }
-        dataResult = dataResult.replace("\n","");
+        dataResult = dataResult.replace("\n","").replace("<","[").replace(">","]");
         JsonResult result = JacksonUtils.genJsonResult(ConstUtils.SUCCESS, dataResult);
         return JacksonUtils.callback(CommonUtils.getCurrentRequest(), JacksonUtils.toJson(result));
     }
